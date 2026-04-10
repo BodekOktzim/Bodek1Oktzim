5d6a59abd966c176b85d7eeeff474ca827cea833
BodekOktzim <bodek1oktzim@gmail.com>
2026-04-10 16:28:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,38 @@
-self.addEventListener('install', e=>{
-  console.log('Service Worker Installed');
+const CACHE_NAME = 'beitar-oktzim-v1';
+// רשימת הקבצים שאנחנו רוצים לשמור בזיכרון של הטלפון
+const assets = [
+  './index.html',
+  './Bodek1Oktzim.png',
+  './manifest.json'
+];
+
+// התקנה - שומר את הקבצים ב-Cache
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets...');
+      return cache.addAll(assets);
+    })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e=>{
-  console.log('Service Worker Activated');
+// הפעלה - ניקוי קבצים ישנים אם היו
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
-self.addEventListener('fetch', e=>{
-  // כאן אפשר להוסיף קאשינג בעתיד
+// שליפת נתונים - אם אין אינטרנט, קח מהזיכרון
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
 });
